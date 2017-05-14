33c1f1fbfab636c9e82598fbe0596268be22983c
mahdiyaroraei <moa15928@gmail.com>
2017-05-14 18:05:13 +0430

    free code generate and use

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ $app->get('pay/{license_id}' , 'BuyController@pay');
 $app->get('payed/{license_id}' , 'BuyController@payed');
 $app->post('activation' , 'BuyController@activation');
 $app->post('deactive' , 'BuyController@deactive');
+$app->get('generatecode' , 'BuyController@generatecode');
+$app->post('freeactivation' , 'BuyController@freeactivation');
